b9937632decdfd33396017d98d7cbd0556871d02
Techo10n <trafficjammisterowl@gmail.com>
2024-09-24 23:08:24 -0400

    .

--- website/app/api/spotify/auth/route.js
+++ website/app/api/spotify/auth/route.js
@@ -7,7 +7,7 @@ if (!code) {
 } else {
     const accessToken = await getAccessToken(clientId, code);
     const profile = await fetchProfile(accessToken);
-    populateUI(profile);
+    console.log(profile);
 }
 
 export async function redirectToAuthCodeFlow(clientId) {
@@ -73,20 +73,4 @@ async function fetchProfile(token) {
     });
 
     return await result.json();
-}
-
-function populateUI(profile) {
-    document.getElementById("displayName").innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
-    }
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
 }
